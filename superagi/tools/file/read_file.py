d768c55316e32448d09b9a49b466ecc5d7b91ee2
PeteyPete <peterpipernz1@gmail.com>
2023-06-25 04:28:01 +1000

    This code should replace the existing `_execute` method in the `ReadFileTool` class in 'read_file.py'

--- superagi/tools/file/read_file.py
+++ superagi/tools/file/read_file.py
@@ -38,13 +38,14 @@ class ReadFileTool(BaseTool):
         input_root_dir = get_config('RESOURCES_INPUT_ROOT_DIR')
         output_root_dir = get_config('RESOURCES_OUTPUT_ROOT_DIR')
         final_path = None
-         # Check in input_root_dir
+
+        # Check in input_root_dir and its subdirectories
         for dirpath, dirnames, filenames in os.walk(input_root_dir):
             if file_name in filenames:
                 final_path = os.path.join(dirpath, file_name)
                 break
 
-        # If not found, check in output_root_dir
+        # If not found, check in output_root_dir and its subdirectories
         if final_path is None:
             for dirpath, dirnames, filenames in os.walk(output_root_dir):
                 if file_name in filenames:
@@ -54,24 +55,7 @@ class ReadFileTool(BaseTool):
         if final_path is None:
             raise FileNotFoundError(f"File '{file_name}' not found.")
 
-        if input_root_dir is not None:
-            input_root_dir = input_root_dir if input_root_dir.startswith("/") else os.getcwd() + "/" + input_root_dir
-            input_root_dir = input_root_dir if input_root_dir.endswith("/") else input_root_dir + "/"
-            final_path = input_root_dir + file_name
-
-        if final_path is None or not os.path.exists(final_path):
-            if output_root_dir is not None:
-                output_root_dir = output_root_dir if output_root_dir.startswith(
-                    "/") else os.getcwd() + "/" + output_root_dir
-                output_root_dir = output_root_dir if output_root_dir.endswith("/") else output_root_dir + "/"
-                final_path = output_root_dir + file_name
-
-        if final_path is None or not os.path.exists(final_path):
-            raise FileNotFoundError(f"File '{file_name}' not found.")
-
-        directory = os.path.dirname(final_path)
-        os.makedirs(directory, exist_ok=True)
-
         with open(final_path, 'r') as file:
             file_content = file.read()
-        return file_content[:1500]
+
+        return file_content[:2000]
