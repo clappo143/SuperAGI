cd6682d9815e0b85742a4f403322d1a770d2f081
PeteyPete <peterpipernz1@gmail.com>
2023-07-10 12:26:30 +1000

    bye

--- tests/tools/test_google_serp.py
+++
@@ -1,8 +0,0 @@
-from superagi.tools.google_serp_search import GoogleSerpTool
-
-def test_search_run():
-    tool = GoogleSerpTool(api_key='4ff36b0914cedf280222c47528cb1fdb7f19c1fc')
-    result = tool.search_run('Apple')
-    assert 'snippets' in result
-    assert 'links' in result
-    # Add more assertions here based on what you expect the output to be
